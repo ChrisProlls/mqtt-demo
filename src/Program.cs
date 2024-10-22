3ffa53fbcca4f95a0d66c2605d2d79e2e8afb834
Christophe Gigax <christophe.gigax@outlook.com>
2024-10-22 22:53:55 +0200

    WORKING !

--- src/Program.cs
+++ src/Program.cs
@@ -4,17 +4,18 @@ using MQTTnet.Client;
 using MQTTnet.Formatter;
 
 string x509_pem = @"C:\Users\pc\Documents\_Tmp\MQTTCertificate\openssl.crt.pem";  //Provide your client certificate .cer.pem file path
-//string x509_key = @"C:\Users\pc\Documents\_Tmp\MQTTCertificate\openssl.key.pem";  //Provide your client certificate .key.pem file path
+string x509_key = @"C:\Users\pc\Documents\_Tmp\MQTTCertificate\openssl.key.pem";  //Provide your client certificate .key.pem file path
 
-//var certificate = new X509Certificate2(X509Certificate2.CreateFromPemFile(x509_pem, x509_key).Export(X509ContentType.Pkcs12));
+// Load certificate and private key from PEM files
+var certificate = new X509Certificate2(X509Certificate2.CreateFromPemFile(x509_pem, x509_key).Export(X509ContentType.Pkcs12));
 
-var certificate = File.ReadAllText(x509_pem);
+// Add the loaded certificate to a certificate collection
+X509Certificate2Collection certificates = new X509Certificate2Collection
+{
+    certificate
+};
 
 Console.WriteLine("Getting certificate");
-Console.WriteLine(certificate);
-
-X509Certificate2Collection caChain = new X509Certificate2Collection();
-caChain.ImportFromPem(certificate);
 
 var mqttFactory = new MqttFactory();
 
@@ -22,13 +23,14 @@ using (var mqttClient = mqttFactory.CreateMqttClient())
 {
     // Use builder classes where possible in this project.
     var mqttClientOptions = new MqttClientOptionsBuilder()
-        .WithTcpServer("demomqtt.francecentral-1.ts.eventgrid.azure.net")
-        .WithProtocolVersion(MqttProtocolVersion.V500)
+        .WithTcpServer("demomqtt.francecentral-1.ts.eventgrid.azure.net", 8883)
+        //.WithProtocolVersion(MqttProtocolVersion.V500)
         .WithClientId("demo-mqtt")
         .WithCredentials("demo-mqtt", "")
         .WithTlsOptions(new MqttClientTlsOptionsBuilder()
-                    .WithTrustChain(caChain)
-                    .Build())
+                .WithSslProtocols(System.Security.Authentication.SslProtocols.Tls12 | System.Security.Authentication.SslProtocols.Tls13)
+                .WithClientCertificates(certificates) // Missed to update the client certificate provider details**
+                .Build())
         .Build();
 
     // This will throw an exception if the server is not available.
